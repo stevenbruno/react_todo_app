958999865c7578cc1917d4a9d4548e5fa1eabec1
Steven Bruno <sbruno636@gmail.com>
2019-11-12 20:14:24 -0600

    pass todos down as state from app to Todos

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 
 class Todos extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>App</h1>
-      </div>
-    );
+    return this.props.todos.map((todo) => (
+    <h3>{ todo.title }</h3>
+    ))
   }
 }
 
